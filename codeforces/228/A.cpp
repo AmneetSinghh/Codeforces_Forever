206eb5dae3bd938d5faf00f41e37abbfc251b54d
Harry Singh <harryamneet600@gmail.com>
2019-07-10 09:38:00 +0530

    Add solution for problem `A - Is your horseshoe on the other hoof?` Link: https://codeforces.com/contest/228/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/228/submission/56777969

---
+++ codeforces/228/A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+ set<long> s;
+ long a,b,c,d;
+ cin>>a>>b>>c>>d;
+ s.insert(a);
+  s.insert(b);
+   s.insert(c);
+    s.insert(d);
+    cout<<(4-s.size())<<endl;
+ 
+  
+
+}
+
+
+
+
+
+
