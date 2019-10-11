980e2a0c047d82f8ac7f6deaf0c42e9857df66b9
Harry Singh <harryamneet600@gmail.com>
2019-10-11 11:44:00 +0530

    Add solution for problem `A - Prime Subtraction` Link: https://codeforces.com/contest/1238/problem/A Tags: math, number theory, *900 Ref: https://codeforces.com/contest/1238/submission/62343152

---
+++ codeforces/1238/A.cpp
@@ -0,0 +1,27 @@
+//satyaki3794
+#include <bits/stdc++.h>
+#define ff first
+#define ss second
+#define pb push_back
+#define MOD (1000000007LL)
+#define LEFT(n) (2*(n))
+#define RIGHT(n) (2*(n)+1)
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+int main()
+{
+	
+	int t;cin>>t;
+	while(t--){
+	ll a,b;
+	cin>>a>>b;
+	if(a-b>1)
+	cout<<"YES\n";
+	else
+	cout<<"NO\n";
+	}
+}
