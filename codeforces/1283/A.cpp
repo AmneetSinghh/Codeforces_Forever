c4f2aa250b89cbd0f55f18adc24403cd91b9072e
Harry Singh <harryamneet600@gmail.com>
2020-01-04 15:49:00 +0530

    Add solution for problem `A - Minutes Before the New Year` Link: https://codeforces.com/contest/1283/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1283/submission/68159598

---
+++ codeforces/1283/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+int t;cin>>t;
+while(t--)
+{
+	int h,m;
+	cin>>h>>m;
+	int hours=24-h;
+	int min=60-m;
+	int tp=((hours-1)*60)+min;
+	cout<<tp<<endl;
+	
+	
+}
+
+
+
+
+
+
+
+	
+}
