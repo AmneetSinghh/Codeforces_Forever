91b63e641fc2765601dff8ebd9d12b370570d6e0
Harry Singh <harryamneet600@gmail.com>
2020-05-11 02:17:00 +0530

    Add solution for problem `C - Mind Control` Link: https://codeforces.com/contest/1291/problem/C Tags: brute force, data structures, greedy, math, *1600 Ref: https://codeforces.com/contest/1291/submission/79690247

---
+++ codeforces/1291/C.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int nbTests; cin >> nbTests;
+    while (nbTests--) {
+	    int nbElem, pos, nbControl;
+	    cin >> nbElem >> pos >> nbControl;
+
+	    int before = pos-1, behind = nbElem-pos;
+	    nbControl = min(nbControl, before);
+
+	    vector<int> tab(nbElem);
+	    for (int i = 0; i < nbElem; ++i) cin >> tab[i];
+
+	    int bestStrategy = 0;
+
+	    for (int orderFirst = 0; orderFirst <= nbControl; ++orderFirst) {
+		    int strategyAns = (int)(1e9) + 5;
+		    for (int opponentFirst = 0; opponentFirst <= before-nbControl; ++opponentFirst) {
+			    int caseAns = max(tab[orderFirst + opponentFirst], tab[orderFirst + opponentFirst + behind]); 
+			    strategyAns = min(strategyAns, caseAns);
+		    }
+		    bestStrategy = max(bestStrategy, strategyAns);
+	    }
+
+	    cout << bestStrategy << "\n";
+    }
+}
