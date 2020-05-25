3450e301692d98a925dff53bf5dc3ec856cbc36b
Harry Singh <harryamneet600@gmail.com>
2020-05-25 09:58:00 +0530

    Add solution for problem `H - Binary Median` Link: https://codeforces.com/contest/1360/problem/H Tags: binary search, bitmasks, brute force, constructive algorithms, *2100 Ref: https://codeforces.com/contest/1360/submission/81346173

---
+++ codeforces/1360/H.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+ 
+#define u_map unordered_map
+#define u_set unordered_set
+ 
+using ll = long long;
+ 
+using namespace std;
+ 
+ll powi(ll x, ll y){
+    ll ans = 1;
+    while(y--){
+        ans *= x;
+    }
+    return ans;
+}
+ 
+void solve(){
+    ll n, m;
+    cin >> n >> m;
+    ll k = powi(2, m)-n;
+    ll median = (k-1)/2;
+    vector<ll> removed(n, 0);
+    for(int i = 0; i < n; i++)
+    {
+        string s;
+        cin >> s;
+        removed[i] = stoll(s, 0, 2);/// this means i have to convert string to base to
+    }
+    sort(removed.begin(), removed.end());
+    for(ll i : removed)
+        if(i <= median)
+            median++;
+            
+            /// convert mediam to binary bro
+    string out = bitset<60>(median).to_string();//// make the bitset length 60 bro
+    out = out.substr(out.size()-m, m);
+    cout << out << endl;
+}
+ 
+int main(){
+    int tc;
+    cin >> tc;
+    while(tc--){
+        solve();
+    }
+    return 0;
+}
