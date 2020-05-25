66823f508ce232ef3b2e91abc11a6a60bd1f1296
Harry Singh <harryamneet600@gmail.com>
2020-05-25 19:39:00 +0530

    Add solution for problem `C - Shockers` Link: https://codeforces.com/contest/907/problem/C Tags: bitmasks, implementation, strings, *1600 Ref: https://codeforces.com/contest/907/submission/81390727

--- codeforces/907/C.cpp
+++ codeforces/907/C.cpp
@@ -4,27 +4,32 @@ using namespace std;
 int main(){
     int n, cnt = 0;
     set<char> pos;
-    for(char i = 'a'; i <= 'z'; i++) 
-	pos.insert(i);
+    for(char i = 'a'; i <= 'z'; i++) pos.insert(i);
     cin >> n;
     for(int i = 0; i < n;i++){
-        char o;string w;
-        cin >> o>>w;
-        if(o == '.')
-		{
+        char o;
+        cin >> o;
+ 
+        if(o == '.'){
+           string w;
+           cin >> w;
            for(char a : w) 
 		   pos.erase(a);
         }
         else if(o == '!'){
             set<char> x;
+            string w;
+            cin >> w;
             if(pos.size() == 1){cnt++; continue;}
+            
             for(char a : w) x.insert(a);
-            //abcdsg
             for(char i = 'a'; i <= 'z'; i++) if(!x.count(i)) pos.erase(i);
         }
         else {
             if(pos.size() == 1 and i != n - 1) cnt++;
-            pos.erase(w[0]);
+            char c;
+            cin >> c;
+            pos.erase(c);
         }
     }
     cout << cnt << endl;
