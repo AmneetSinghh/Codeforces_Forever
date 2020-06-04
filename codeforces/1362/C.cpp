ca47a640a897aa2652a1dcbb0966c6e576712a21
Harry Singh <harryamneet600@gmail.com>
2020-06-05 00:45:00 +0530

    Add solution for problem `C - Johnny and Another Rating Drop` Link: https://codeforces.com/contest/1362/problem/C Tags: bitmasks, greedy, math, *1400 Ref: https://codeforces.com/contest/1362/submission/82572435

--- codeforces/1362/C.cpp
+++ codeforces/1362/C.cpp
@@ -1,25 +1,30 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+#define int unsigned long long int
+int ans[65];
+int32_t main(){
+    
+    ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
 
-typedef long long LL;
-
-void solve(){
-	LL a;
-	scanf("%lld", &a);
-	
-	LL ans = 0;
-	for(int i = 0; i < 60; ++i)
-		if(a & (1LL << i))
-			ans += (1LL << (i + 1)) - 1;
-	printf("%lld\n", ans);
-}
-
-int main(){
-	int quest;
-	scanf("%d", &quest);
-	
-	while(quest--)
-		solve();
-	return 0;
+ans[0]=1;
+int res=2;;
+ for(int i=1;i<=63;i++){
+ 	res*=2;
+ 	ans[i]=res-1;
+ }
+ 
+ 
+	int T;
+	cin >> T;while(T--){
+		int N,sol=0; cin >> N;
+		for(int i = 0; i < 62; ++i){
+			if(N & (1ll<<i)){
+				sol += ans[i];
+				continue;
+			}
+		}
+	cout<<sol<<"\n";//1152921504606846976
+	}
+         
+    return 0;
 }
