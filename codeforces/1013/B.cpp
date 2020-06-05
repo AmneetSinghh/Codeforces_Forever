ab4a987707f1109aa8ca113bc49e60d005f463fe
Harry Singh <harryamneet600@gmail.com>
2020-06-05 19:12:00 +0530

    Add solution for problem `B - And` Link: https://codeforces.com/contest/1013/problem/B Tags: greedy, *1200 Ref: https://codeforces.com/contest/1013/submission/82635163

---
+++ codeforces/1013/B.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+ #define pb push_back
+#define int long long
+#define endl '\n'
+#define maxn 500005
+
+int32_t main(){
+
+int n,k;
+cin>>n>>k;
+int a[100005];
+int mp[100005]={0};
+for(int i=1;i<=n;i++){
+cin>>a[i];mp[a[i]]++;}
+int f=0;
+for(int i=0;i<=100002;i++)
+if(mp[i]>=2)f=1;
+if(f==1)
+cout<<"0\n";
+else
+{
+	for(int i=1;i<=n;i++)
+	{
+		int value=a[i]&k;
+		if(value==a[i])continue;
+		if(mp[value]){f=1;break;}
+	}
+	if(f==1)
+	cout<<"1\n";
+	else 
+	{
+		int hash[100005]={0};
+		for(int i=1;i<=n;i++){
+		int value=a[i]&k;
+		hash[value]++;}
+		
+		for(int i=0;i<=100002;i++){
+		if(hash[i]>1){f=1;break;}}
+		
+		if(f==1)
+		cout<<"2\n";
+		else 
+		cout<<"-1\n";
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+}
+
