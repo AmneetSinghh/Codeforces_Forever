5def3dedafab068caf062458fc046fb334b67836
Harry Singh <harryamneet600@gmail.com>
2020-08-25 02:26:00 +0530

    Add solution for problem `C - Save Energy!` Link: https://codeforces.com/contest/937/problem/C Tags: math, *1700 Ref: https://codeforces.com/contest/937/submission/90858671

--- codeforces/937/C.cpp
+++ codeforces/937/C.cpp
@@ -1,25 +1,16 @@
-// /Code by : Harry_Singh/
 #include<bits/stdc++.h>
-#define int unsigned long long int  
-#define double double
-#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
-#define ipair pair<int,int>
-#define pb push_back
-#define f(i,n) for(int i=0;i<n;i++)
-#define fo(i,n) for(int i=n;i>=0;i--)
-#define F(i,n) for(int i=1;i<=n;i++)
-#define FO(i,n) for(int i=n;i>=1;i--)
-#define mod 1000000007
 using namespace std;
-#define inf 100000000000000009
-#define tr cout<<"\n" 
-#define br break
-#define N 10001
-// directions
-
-void we_have_choices()
+#define tr cout<<endl
+ 
+int main()
 {
-  double k, d, t;
+
+#ifndef ONLINE_JUDGE
+ freopen("input.txt","r",stdin);
+ freopen("output.txt","w",stdout);
+#endif
+
+    double k, d, t;
     cin >> k >> d >> t;
     double block_m; // how many minutes in one block
     double temp_m, temp_m_2; // how many on minutes in one block, off .......
@@ -34,10 +25,15 @@ void we_have_choices()
         if (t_ * d != k) block_m += d;
         temp_m = k;
         temp_m_2 = block_m - temp_m;
+        // Ithe tak aram nal smj gya
     }
     double block = (temp_m / t) + (temp_m_2 / (2 * t)); // how much(fraction) done in one block
     double x;
     x = floor(1.0 / block); // how many blocks
+
+
+// done sm gya baby mere
+ 
     double done = x * block;
     double left = 1 - done;/// Left baby
     double dn_on = temp_m / t;
@@ -53,19 +49,7 @@ void we_have_choices()
 
     // making the x and the blok_m is very easy
     printf("%.12lf\n", x * block_m + extra);
-}
+    //cout << setprecision(12) << fixed << x * block_m + extra << endl;
  
- 
- 
- 
-int32_t main()
-{
-#ifndef ONLINE_JUDGE
- freopen("input.txt","r",stdin);
- freopen("output.txt","w",stdout);
-#endif
-    io;
-    int g=1;
-    while (g--)we_have_choices();
     return 0;
 }
