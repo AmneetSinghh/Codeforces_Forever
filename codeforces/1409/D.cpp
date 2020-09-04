3ab52ece64da2665b59e9928ebde4191898491e3
Harry Singh <harryamneet600@gmail.com>
2020-09-05 01:11:00 +0530

    Add solution for problem `D - Decrease the Sum of Digits` Link: https://codeforces.com/contest/1409/problem/D Tags: greedy, math, *1500 Ref: https://codeforces.com/contest/1409/submission/91890582

---
+++ codeforces/1409/D.cpp
@@ -0,0 +1,80 @@
+/*
+Name                     : Harry Singh (:)
+Age                      : 20 (:)
+College                  : Lovely Professional University (:)
+Email                    : harryamneet600@gmail.com (:)
+Hobbies                  : Coding and Typeracer (:)
+Goal                     : Long term is fixed and Short term is changing everyday (:)
+Relatoinship Status      : Single (:)
+Phone no                 : 91-********77 (:)
+Waheguru Ji ka Khalsa, Waheguru Ji ki Fateh;
+*/
+#include<bits/stdc++.h>
+#define int long long
+#define double double
+#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define ipair pair<int,int>
+#define pb push_back
+#define f(i,n) for(int i=0;i<n;i++)
+#define fo(i,n) for(int i=n;i>=0;i--)
+#define F(i,n) for(int i=1;i<=n;i++)
+#define f_a(a,i,n) for(int i=a;i<=n;i++)
+#define f_b(a,i,b) for(int i=a;i<=b;i++)
+#define FO(i,n) for(int i=n;i>=1;i--)
+#define mod 1000000007
+using namespace std;
+#define tr cout<<"\n" 
+#define br break
+const int N = 2*1e5+7;
+int inf=1e17;
+
+int sum(int  x){
+    int  p=0;
+    while(x){
+        p+=x%10;
+        x/=10;
+    }
+    return p;
+}
+ 
+void Waheguru()
+{
+int n,s;
+cin>>n>>s;
+int cost=0,y=1;
+while(sum(n)>s)// Just remove the degits baby
+{
+  
+  int current_last=n%10;
+  if(current_last==0)
+  {
+    y*=10;
+    n/=10;// N is decreased.
+    continue;
+  }
+  int a=10-current_last;
+  cost+=a*y;
+  n+=a;
+}
+cout<<cost;
+tr;
+
+
+
+}
+
+
+ 
+ 
+ 
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+ freopen("input.txt","r",stdin);
+ freopen("output.txt","w",stdout);
+#endif
+    io;
+    int g;cin>>g;
+    while (g--)Waheguru();
+    return 0;
+}
