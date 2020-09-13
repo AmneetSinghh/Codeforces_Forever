750a3dcf1cd095f4d4a82ef1ef7a76c044034011
Harry Singh <harryamneet600@gmail.com>
2020-09-13 11:26:00 +0530

    Add solution for problem `B - Mahmoud and Ehab and the bipartiteness` Link: https://codeforces.com/contest/862/problem/B Tags: dfs and similar, graphs, trees, *1300 Ref: https://codeforces.com/contest/862/submission/92684955

---
+++ codeforces/862/B.cpp
@@ -0,0 +1,76 @@
+/*
+Name                     : Harry Singh (:)
+Age                      : 20 (:)
+College                  : Lovely Professional University (:)
+Email                    : harryamneet600@gmail.com (:)
+Hobbies                  : Coding and Typeracer (:)
+Goal                     : Long term is fixed and Short term is changing everyday (:)
+Relatoinship Status      : Single (:)
+Phone no                 : 91-********77 (:)
+Waheguru Ji ka Khalsa, Waheguru Ji ki Fateh;
+*/
+#include<bits/stdc++.h>
+#define int long long
+#define ll long long
+#define double double
+#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define ipair pair<int,int>
+#define pb push_back
+#define f(i,n) for(int i=0;i<n;i++)
+#define fo(i,n) for(int i=n;i>=0;i--)
+#define F(i,n) for(int i=1;i<=n;i++)
+#define f_a(a,i,n) for(int i=a;i<=n;i++)
+#define f_b(a,i,b) for(int i=a;i<=b;i++)
+#define FO(i,n) for(int i=n;i>=1;i--)
+#define mod 1000000007
+using namespace std;
+#define tr cout<<"\n" 
+#define br break
+#define N 100005 
+#define inf 10000000000000009
+#define br break
+/*  I know I have that Ability, I can do anything, 
+I'll be The _Candidate Master_ 
+(Patience and Practice)  */
+ 
+vector<int> G[100005];
+int dp[2]={0};
+void dfs(int s,int p,int color)
+{
+  dp[color]++;// count of zeros an d ones
+  for(auto c:G[s])
+  {
+    if(c==p)continue;
+    dfs(c,s,!color);
+  }
+}
+void Waheguru()
+{
+ 
+int n;
+cin>>n;
+F(i,n-1)
+{
+  int u,v;
+  cin>>u>>v;
+  G[u].pb(v);
+  G[v].pb(u);
+ 
+}
+dfs(1,0,0);
+cout<<dp[0]*dp[1]-(n-1);
+tr;
+}
+ 
+ 
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+ freopen("input.txt","r",stdin);
+ freopen("output.txt","w",stdout);
+#endif
+    io;
+    int g=1;
+    while (g--)Waheguru();
+    return 0;
+}
