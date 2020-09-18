da02adc8e989bc5d861de366ab370739b9893be0
Harry Singh <harryamneet600@gmail.com>
2020-09-18 14:24:00 +0530

    Add solution for problem `D1 - Parallel Universes (Easy)` Link: https://codeforces.com/contest/1184/problem/D1 Tags: implementation, *1600 Ref: https://codeforces.com/contest/1184/submission/93102237

---
+++ codeforces/1184/D1.cpp
@@ -0,0 +1,82 @@
+/*
+Name                     : Harry Singh (:)
+Age                      : 20 (:)
+College                  : Lovely Professional University (:)
+Email                    : harryamneet600@gmail.com (:)
+Hobbies                  : Coding and Typeracer (:)
+Goal                     : Long term is fixed and Short term is changing everyday (:)
+Relatoinship Status      : Single (:)
+Phone no                 : 91-********77 (:)
+Waheguru Ji ka Khalsa, Waheguru Ji ki Fateh;
+*/
+#include<bits/stdc++.h>
+#define int long long
+#define ll long long
+#define double double
+#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define ipair pair<int,int>
+#define pb push_back
+#define f(i,n) for(int i=0;i<n;i++)
+#define fo(i,n) for(int i=n;i>=0;i--)
+#define F(i,n) for(int i=1;i<=n;i++)
+#define f_a(a,i,n) for(int i=a;i<=n;i++)
+#define f_b(a,i,b) for(int i=a;i<=b;i++)
+#define FO(i,n) for(int i=n;i>=1;i--)
+#define mod 1000000007
+using namespace std;
+#define tr cout<<"\n" 
+#define br break
+#define N 200005 
+#define inf 10000000000000009
+#define br break
+/*  I know I have that Ability, I can do anything, 
+I'll be The _Candidate Master_ 
+(Patience and Practice)  */
+
+
+
+
+
+
+// set is  {1,X}, {2,3,X},  {4,5,6,7,X}  <- X means not choose any
+
+void Waheguru()
+{
+   int n,k,m,t;
+   cin>>n>>k>>m>>t;
+   F(i,t)
+   {
+           int a,b;
+           cin>>a>>b;
+           if(a==0)
+           {
+               if(b<k)n-=b,k-=b;
+               else if(b==k)k=b,n=b;
+               else if(b>k)n-=(n-b);
+           }
+           else
+           {
+               if(b<=k)++k;
+               ++n;
+           }
+
+           cout<<n<<" "<<k;tr;
+   }
+}
+
+
+
+
+
+
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+ freopen("input.txt","r",stdin);
+ freopen("output.txt","w",stdout);
+#endif
+    io;
+    int g=1;
+    while (g--)Waheguru();
+    return 0;
+}
