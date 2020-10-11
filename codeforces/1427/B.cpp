211a1842803f655a75953b6e346f73bffa54bda6
Harry Singh <harryamneet600@gmail.com>
2020-10-11 13:52:00 +0530

    Add solution for problem `B - Chess Cheater` Link: https://codeforces.com/contest/1427/problem/B Tags: greedy, implementation, sortings, *1400 Ref: https://codeforces.com/contest/1427/submission/95189527

---
+++ codeforces/1427/B.cpp
@@ -0,0 +1,135 @@
+/*
+|********************************************** Not_Stable *********************************************************|                                                                |
+|   **     *   *******  *********                                                                                   |
+|   * *    *  *      *      *                                                                                       |
+|   *  *   *  *      *      *                                                                                       |
+|   *   *  *  *      *      *                                                                                       |
+|   *    * *  *      *      *                                                                                       |
+|   *     **   ******       *                                                                                       |
+|                            ******    **********   ******    ******  *         *******                             |
+|                            *             *       *      *  *    *   *         *                                   |
+|                             ******       *       *      *  *  **    *         *******                             |
+|                                  *       *       * **** *  *    *   *         *                                   |
+|                                 *        *       *      *  *     *  *         *                                   |
+|                            *****         *       *      *   ******   *******  *******                             |
+|Keep it Simple!                                                                                                    |
+|********************************************** Not_Stable ********************************************************* 
+*/
+#include<bits/stdc++.h>
+#define int long long
+#define ll long long
+#define double double
+#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define ipair pair<int,int>
+#define pb push_back
+#define f(i,n) for(int i=0;i<n;i++)
+#define fo(i,n) for(int i=n;i>=0;i--)
+#define F(i,n) for(int i=1;i<=n;i++)
+#define f_a(a,i,n) for(int i=a;i<=n;i++)
+#define f_b(a,i,b) for(int i=a;i<=b;i++)
+#define FO(i,n) for(int i=n;i>=1;i--)
+#define tr cout<<"\n" 
+#define br break
+#define all(v) v.begin(),v.end()
+#define sz(b) b.size()
+#define maxn 1000010
+// #define mod 1000000007
+#define inf 1000000000000009
+using namespace std;
+/*  I know I have that Ability, I can do anything, 
+I'll be The _Candidate Master_ 
+(Patience and Practice)  */
+ 
+/*****************************************Things to remember in Contests*******************************************
+          set->  find ,insert,erase   is logn;                 All the things are ordered 
+          map-> searching in map is O(1) and others are log(n);   All the things are ordered by values smallest to largest
+unordered_set->  find ,insert,erase     O(1);
+unordered_map->   find,insert,erase     O(1);
+So we have to find fast like in O(1) use the unordered_set or unordered_map
+NOTES*******************************************************************************
+
+1.we can compare two vectors like   v1>v2 and v1==v2 it is also a same condition for v 2d vector,
+2.For a fraction p/q, if we prove 1/q is finite, then p/q must be finite. 
+so after reduction of the fraction, it will be not relevant with the numerator p
+3. BY dividing 2 numbers  by their GCD we can make both COPRIME- because we remove the common divisor byby;
+
+*/  
+
+// For checking two numbers has the same prime divisors or not work in log(a);
+const int MOD = 998244353;
+const int N = 1e6 + 7;
+int n,k,m;
+int Same_Prime_Divisors_Or_Not(int a,int b)
+{
+	b=__gcd(a,b);
+	while(b>1)
+	{a/=b,b=__gcd(a,b);}
+    if(a==1)return 1;
+    else return 0;
+}
+int add(int a, int b) {
+	a += b;
+	if (a >= MOD) a -= MOD;
+	return a;
+}
+int mul(int a, int b) {
+	return a * b % MOD;
+}
+int binpow(int a, int b) {
+	int res = 1;
+	while (b) {
+		if (b & 1) res = mul(res, a);
+		a = mul(a, a);
+		b >>= 1;
+	}
+	return res;
+}
+
+
+
+
+void Not_Stable() {
+int n,k;cin>>n>>k;
+int total=0;string s;cin>>s;
+int wins=0;
+int pre=-1;std::vector<int> v;
+for(int i=0;i<s.size();i++)
+{
+    if(s[i]=='W')
+    {
+    	++total;
+    	if(i-1>=0 && s[i-1]=='W')++total;
+    	if(pre==-1)pre=i;
+    	else if(i-pre-1>0)v.pb(i-pre-1);
+    	pre=i;
+    }else ++wins;
+}
+
+if(total==0)cout<<max(k*2-1,0ll);
+else if(k>=wins)cout<<max(n*2-1,0ll);
+else
+{
+	sort(all(v));// Now check;
+	int flag=0;
+	for(auto c:v)
+	{
+	   if(k>=c)total+=(c*2+1),k-=c;
+	   else break;
+	}
+	total+=(k*2);
+	cout<<total;
+}
+tr;
+}
+
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+ freopen("input.txt","r",stdin);
+ freopen("output.txt","w",stdout);
+#endif         
+    io;///100000// zeros
+    int g;cin>>g;
+    while (g--)Not_Stable();
+    return 0;
+}
