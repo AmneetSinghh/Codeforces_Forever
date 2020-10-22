8e1e8e692f73a5ed60119cc98c0085fb41ccbb93
Harry Singh <harryamneet600@gmail.com>
2020-10-22 23:16:00 +0530

    Add solution for problem `E - Sergey and Subway` Link: https://codeforces.com/contest/1060/problem/E Tags: dfs and similar, dp, trees, *2000 Ref: https://codeforces.com/contest/1060/submission/96388818

---
+++ codeforces/1060/E.cpp
@@ -0,0 +1,97 @@
+/*
+|********************************************** Not_Stable *********************************************************|                                                                |
+|   **     *   *******  *********                                                                                   |
+|   * *    *  *      *      *                                                                                       |
+|   *  *   *  *      *      *                                                                                       |
+|   *   *  *  *      *      *                                                                                       |
+|   *    * *  *      *      *                                                                                       |
+|   *     **   ******       *                                                                                       |
+|                            ******    **********   ******    ******  *         *******                             |
+|                            *             *       *      *  *    *   *         *                                   |
+|                             ******       *       *      *  *  **    *         *******                             |
+|                                  *       *       * **** *  *    *   *         *                                   |
+|                                 *        *       *      *  *     *  *         *                                   |
+|                            *****         *       *      *   ******   *******  *******                             |
+|Keep it Simple!                                                                                                    |
+|********************************************** Not_Stable ********************************************************* 
+*/
+#include<bits/stdc++.h>
+#define int long long
+#define ll long long
+#define double double
+#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define ipair pair<int,int>
+#define pb push_back
+#define f(i,n) for(int i=0;i<n;i++)
+#define fo(i,n) for(int i=n;i>=0;i--)
+#define F(i,n) for(int i=1;i<=n;i++)
+#define f_a(a,i,n) for(int i=a;i<=n;i++)
+#define f_b(a,i,b) for(int i=a;i<=b;i++)
+#define FO(i,n) for(int i=n;i>=1;i--)
+#define tr cout<<"\n" 
+#define br break
+#define all(v) v.begin(),v.end()
+// #define sz(b) b.size()
+#define maxn 1000010
+// #define mod 1000000007
+#define inf 1000000000000009
+using namespace std;
+/*  I know I have that Ability, I can do anything, 
+I'll be The _Candidate Master_ 
+(Patience and Practice)  */
+ 
+/*****************************************Things to remember in Contests*******************************************
+          set->  find ,insert,erase   is logn;                 All the things are ordered 
+          map-> searching in map is O(1) and others are log(n);   All the things are ordered by values smallest to largest
+unordered_set->  find ,insert,erase     O(1);
+unordered_map->   find,insert,erase     O(1);
+So we have to find fast like in O(1) use the unordered_set or unordered_map
+NOTES*******************************************************************************
+ 
+1.we can compare two vectors like   v1>v2 and v1==v2 it is also a same condition for v 2d vector,
+2.For a fraction p/q, if we prove 1/q is finite, then p/q must be finite. 
+so after reduction of the fraction, it will be not relevant with the numerator p
+3. BY dividing 2 numbers  by their GCD we can make both COPRIME- because we remove the common divisor by;
+4. If i want to find the distance from each vertex to each vertex answe is :
+   [ans=0; for(i to n): ans+nodes_in_subtree[i]*N-nodes_in_subtree[i]];
+*/  
+const int maxx=200005;
+std::vector<int > G[maxx];
+int sub_nodes[maxx];
+int odd=0;
+void  dfs(int s,int p,int d)
+{
+  if(d&1)++odd;// this will tell how many times odd_distanted path will occur,
+  sub_nodes[s]=1;
+  for(auto c:G[s])
+  {
+  	if(c!=p)dfs(c,s,d+1),sub_nodes[s]+=sub_nodes[c];
+  }
+}
+
+void Not_Stable() 
+{
+int n,u,v;
+cin>>n;
+F(i,n-1)cin>>u>>v,G[u].pb(v),G[v].pb(u);
+dfs(1,0,0);
+int ans=0;
+F(i,n)ans+=sub_nodes[i]*(n-sub_nodes[i]);
+
+ans+=(odd*(n-odd));
+cout<<ans/2;
+tr;
+
+}
+ 
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+ freopen("input.txt","r",stdin);
+ freopen("output.txt","w",stdout);
+#endif         
+    io;///100000// zeros
+    int g=1;
+    while (g--)Not_Stable();
+    return 0;
+}
