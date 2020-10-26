b26701d4af7a3b374dea6913f6e22ce596fe2f7a
Harry Singh <harryamneet600@gmail.com>
2020-10-26 15:10:00 +0530

    Add solution for problem `A - Finding Sasuke` Link: https://codeforces.com/contest/1435/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1435/submission/96763568

---
+++ codeforces/1435/A.cpp
@@ -0,0 +1,84 @@
+/*
+|********************************************** Not_Stable *********************************************************|                                                                |
+|   **     *   *******  *********                                                                                   |
+|   * *    *  *      *      *                                                                                       |
+|   *  *   *  *      *      *                                                                                       |
+|   *   *  *  *      *      *                                                                                       |
+|   *    * *  *      *      *                                                                                       |
+|   *     **   ******       *                                                                                       |
+|                            ******    **********   ******    ******  *         *******                             |
+|                            *             *       *      *  *    *   *         *                                   |
+|                             ******       *       *      *  *  **    *         *******                             |
+|                                  *       *       * **** *  *    *   *         *                                   |
+|                                 *        *       *      *  *     *  *         *                                   |
+|                            *****         *       *      *   ******   *******  *******                             |
+|Keep it Simple!                                                                                                    |
+|********************************************** Not_Stable ********************************************************* 
+*/
+#include<bits/stdc++.h>
+#define int long long
+#define ll long long
+#define double double
+#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define ipair pair<int,int>
+#define pb push_back
+#define f(i,n) for(int i=0;i<n;i++)
+#define fo(i,n) for(int i=n;i>=0;i--)
+#define F(i,n) for(int i=1;i<=n;i++)
+#define f_a(a,i,n) for(int i=a;i<=n;i++)
+#define f_b(a,i,b) for(int i=a;i<=b;i++)
+#define FO(i,n) for(int i=n;i>=1;i--)
+#define tr cout<<"\n"
+#define br break
+#define mod 1000000007
+#define all(v) v.begin(),v.end()
+// #define sz(b) b.size()
+// #define maxn 1000010
+// #define mod 1000000007
+#define inf 1000000000000009
+using namespace std;
+/*  I know I have that Ability, I can do anything,
+I'll be The _Candidate Master_
+(Patience and Practice)  */
+
+/*****************************************Things to remember in Contests*******************************************
+          set->  find ,insert,erase   is logn;                 All the things are ordered
+          map-> searching in map is O(1) and others are log(n);   All the things are ordered by values smallest to largest
+unordered_set->  find ,insert,erase     O(1);
+unordered_map->   find,insert,erase     O(1);
+So we have to find fast like in O(1) use the unordered_set or unordered_map
+NOTES*******************************************************************************
+
+1.we can compare two vectors like   v1>v2 and v1==v2 it is also a same condition for v 2d vector,
+2.For a fraction p/q, if we prove 1/q is finite, then p/q must be finite.
+so after reduction of the fraction, it will be not relevant with the numerator p
+3. BY dividing 2 numbers  by their GCD we can make both COPRIME- because we remove the common divisor by;
+4. If i want to find the distance from each vertex to each vertex answe is :
+   [ans=0; for(i to n): ans+nodes_in_subtree[i]*N-nodes_in_subtree[i]];
+*/
+void Not_Stable(){
+
+	
+	int n;
+	cin>>n;
+	int a[n+1];
+	F(i,n)cin>>a[i];
+	for(int i=n;i>n/2;i--)cout<<a[i]*-1<<" ";
+	for(int i=n/2;i>=1;i--)cout<<a[i]<<" ";
+		tr;
+}
+
+
+
+int32_t main()
+{
+	#ifndef ONLINE_JUDGE
+ freopen("input.txt","r",stdin);
+ freopen("output.txt","w",stdout);
+#endif         
+    io;///100000// zeros
+    // SieveOfEratosthenes();
+    int g;cin>>g;
+    while (g--)Not_Stable();
+    return 0;
+}
