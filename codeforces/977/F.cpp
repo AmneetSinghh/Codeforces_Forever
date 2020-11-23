27bd9cd9f21fe02935996daedc2351d63bb2a6a0
Harry Singh <harryamneet600@gmail.com>
2020-11-23 13:34:00 +0530

    Add solution for problem `F - Consecutive Subsequence` Link: https://codeforces.com/contest/977/problem/F Tags: dp, *1700 Ref: https://codeforces.com/contest/977/submission/99301993

---
+++ codeforces/977/F.cpp
@@ -0,0 +1,130 @@
+
+#include<bits/stdc++.h>
+#define int long long 
+#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define ipair pair<int,int>
+#define pb push_back
+#define F(i, n) for(int i=1;i<=n;i++)
+#define all(v) v.begin(),v.end()
+#define tr cout<<endl;
+#define mod 1000000007
+#define inf 1000000000000009
+using namespace std;
+const int MAXN=200005;
+
+
+int let_count(int x){
+  int ans=0;
+  while(x % 3 == 0){
+    ans++;
+    x /= 3;
+  }
+  return ans;
+}
+bool cmp(ipair &a,ipair &b)
+{
+	if(a.first>b.first)return true;
+	else if(a.first==b.first)
+	{
+          if(a.second<b.second)return true;
+          else return false;
+	}
+	return false;
+}
+
+
+
+
+
+int  n,m;
+vector<int> G[MAXN] ;
+bool vis[MAXN] ;
+vector<int> comp ;
+int ans=0;
+void dfs(int s,int p)
+{
+    vis[s]=true;
+    comp.pb(s);
+    for(auto c:G[s]) 
+    {
+        if(!vis[c])
+        	{
+        		dfs(c,s);
+        	}
+
+        	// else if(c!=p)return true;
+    }
+    // return false;
+}
+
+void compo(){
+   for(int i=1;i<=n;i++)vis[i]=false;
+   for(int i=1;i<=n;i++)
+    {
+        if(!vis[i])
+        {
+        	dfs(i,0);
+            // for(auto sd:comp)cout<<sd<<" ";tr;
+
+            // just we have to cehck the dgerr
+            	int pos=0;
+            for(auto sd:comp)
+            {
+            	if(G[sd].size()!=2)pos=-1;
+            }
+            if(pos==0)ans++;
+            comp.clear();
+
+
+        }
+    }
+    cout<<ans;tr;
+}
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+ freopen("input.txt","r",stdin);
+ freopen("output.txt","w",stdout);
+#endif
+    io;
+    int t=1;
+    while (t--)
+    {
+    int n;cin>> n;
+    int a[n+1];
+    F(i,n)cin>>a[i];
+     map<int,int> m;
+     map<int,int> pm;
+int mx=0,start=0;
+    F(i,n)
+    {
+      int v=a[i];
+      if(m[v])
+      {
+            m[v]=max(m[v],m[v-1]+1);
+      }
+      else    m[v]=m[v-1]+1;
+    }
+    for(auto sd:m)
+    {
+    	if(sd.second>mx)mx=sd.second,start=sd.first;
+    }
+    vector<int> res;
+	for (int i = n; i >= 1; i--) {
+		if (a[i] == start) {
+			res.push_back(i);
+			--start;
+		}
+	}
+	reverse(res.begin(), res.end());
+	
+	cout<<mx;tr;
+	for (auto it : res)cout<<it<<" ";
+		tr;
+
+
+
+
+    }
+
+}
