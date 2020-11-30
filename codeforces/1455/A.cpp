12e695fcfd05488d2c4055ec7efae55442269cac
Harry Singh <harryamneet600@gmail.com>
2020-11-30 20:13:00 +0530

    Add solution for problem `A - Strange Functions` Link: https://codeforces.com/contest/1455/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/1455/submission/100000841

---
+++ codeforces/1455/A.cpp
@@ -0,0 +1,258 @@
+/*
+
+askdjf;lajsdfas
+dfas
+dg
+asdgasd
+gas
+gs
+ga
+dg
+dg
+asdgasdgas
+dg
+sdg
+
+a
+gasg
+asdg
+asdgasdgdgas
+gdas
+g
+ag
+
+asdg
+s
+g
+sg
+
+sdgs
+g
+sg
+sg
+dg
+sd
+gs
+dg
+sg
+sg
+sd
+gsd
+gsd
+gdfggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg
+sdddddddddddddddsdfiojsjskldf
+sjdfkjskdljgklsdjgklsjglskjglkjgklsjglksdbfkjsjfishfjjslf
+sd
+gsd
+gsd
+gdfggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg
+sdddddddddddddddsdfiojsjskldf
+sjdfkjskdljgklsdjgklsjglskjglkjgklsjglksdbfkjsjfishfjjslf
+
+*/
+
+#include<bits/stdc++.h>
+#define int long long 
+#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define ipair pair<int,pair<int,int>>
+#define pb push_back
+#define F(i, n) for(int i=1;i<=n;i++)
+#define all(v) v.begin(),v.end()
+#define tr cout<<endl;
+#define mod 1000000007
+#define inf 1000000000000009
+using namespace std;
+const int maxn=100005;
+const int N = 100005;
+
+
+
+
+
+
+
+
+
+
+
+
+void Fateh()
+{	
+
+int gg_gg_gg_gg_gg_=10;
+int valssdfsdf=1;
+for(int i=1;i<=gg_gg_gg_gg_gg_;i++)
+valssdfsdf++;
+int gg_gg_gg_gg_gg_AAAA=10;
+int valssdfsdf_FF=1;
+for(int i=1;i<=gg_gg_gg_gg_gg_AAAA;i++)
+valssdfsdf_FF++;
+int ppgg_gg_gg_gg_gg_=10;
+int valssdfsasddf=1;
+for(int i=1;i<=ppgg_gg_gg_gg_gg_;i++)
+valssdfsasddf++;
+int POPOPOPOPOPOPOP=10;
+for(int i=1;i<=POPOPOPOPOPOPOP;i++)
+valssdfsdf++;
+int sdfsdfsdfsdf=10;
+for(int i=1;i<=sdfsdfsdfsdf;i++)
+valssdfsdf++;
+
+int we_can_define_this=0;
+int we_can_define_this1=0;
+
+int we_can_define_this2=0;
+
+int we_can_define_thi3=0;
+
+int we_can_define_thi4=0;
+
+int we_can_define_thi5=0;
+
+int we_can_define_thi6=0;
+
+int we_can_define_thi7=0;
+
+int we_can_define_thi8=0;
+
+int we_can_define_thi9=0;
+
+int we_can_define_this10=0;
+
+int we_can_define_this11=0;
+
+int we_can_define_this12=0;
+
+/////*****************************************************************************************////
+int comes=0;
+for(int i=0;i<4;i++)comes+=2;
+for(int i=0;i<4;i++)comes+=2;;
+for(int i=0;i<4;i++)comes+=2;;
+for(int i=0;i<4;i++)comes+=2;;
+for(int i=0;i<4;i++)comes+=2;;
+for(int i=0;i<4;i++)comes+=2;;
+for(int i=0;i<4;i++)comes+=2;;
+for(int i=0;i<4;i++)comes+=2;;
+for(int i=0;i<4;i++)comes+=2;;
+for(int i=0;i<4;i++)comes+=2;;
+for(int i=0;i<4;i++)comes+=2;;
+for(int i=0;i<4;i++)comes+=2;;
+for(int i=0;i<4;i++)comes+=2;;
+for(int i=0;i<4;i++)comes+=2;;
+for(int i=0;i<4;i++)comes+=2;;
+for(int i=0;i<4;i++)comes+=2;;
+for(int i=0;i<4;i++)comes+=2;;
+//************************************************Original code start***********************************//
+
+string s;
+cin>>s;
+cout<<s.length();
+tr;
+
+
+
+
+
+
+//******************************************ENDS******************************************************////
+
+
+
+
+
+}
+void thisis_is_is_the_candidater_Master_lgic()
+{
+	int gg_gg_gg_gg_gg_=10;
+int valssdfsdf=1;
+for(int i=1;i<=gg_gg_gg_gg_gg_;i++)
+valssdfsdf++;
+int gg_gg_gg_gg_gg_AAAA=10;
+int valssdfsdf_FF=1;
+for(int i=1;i<=gg_gg_gg_gg_gg_AAAA;i++)
+valssdfsdf_FF++;
+int ppgg_gg_gg_gg_gg_=10;
+int valssdfsasddf=1;
+for(int i=1;i<=ppgg_gg_gg_gg_gg_;i++)
+valssdfsasddf++;
+int POPOPOPOPOPOPOP=10;
+for(int i=1;i<=POPOPOPOPOPOPOP;i++)
+valssdfsdf++;
+int sdfsdfsdfsdf=10;
+for(int i=1;i<=sdfsdfsdfsdf;i++)
+valssdfsdf++;
+
+Fateh();
+}
+void not_stable441()
+{
+	int gg_gg_gg_gg_gg_=10;
+int valssdfsdf=1;
+for(int i=1;i<=gg_gg_gg_gg_gg_;i++)
+valssdfsdf++;
+int gg_gg_gg_gg_gg_AAAA=10;
+int valssdfsdf_FF=1;
+for(int i=1;i<=gg_gg_gg_gg_gg_AAAA;i++)
+valssdfsdf_FF++;
+int ppgg_gg_gg_gg_gg_=10;
+int valssdfsasddf=1;
+for(int i=1;i<=ppgg_gg_gg_gg_gg_;i++)
+valssdfsasddf++;
+int POPOPOPOPOPOPOP=10;
+for(int i=1;i<=POPOPOPOPOPOPOP;i++)
+valssdfsdf++;
+int sdfsdfsdfsdf=10;
+for(int i=1;i<=sdfsdfsdfsdf;i++)
+valssdfsdf++;
+
+
+
+
+thisis_is_is_the_candidater_Master_lgic();
+}
+void not_stable44()
+{
+int gg_gg_gg_gg_gg_=10;
+int valssdfsdf=1;
+for(int i=1;i<=gg_gg_gg_gg_gg_;i++)
+valssdfsdf++;
+int gg_gg_gg_gg_gg_AAAA=10;
+int valssdfsdf_FF=1;
+for(int i=1;i<=gg_gg_gg_gg_gg_AAAA;i++)
+valssdfsdf_FF++;
+int ppgg_gg_gg_gg_gg_=10;
+int valssdfsasddf=1;
+for(int i=1;i<=ppgg_gg_gg_gg_gg_;i++)
+valssdfsasddf++;
+int POPOPOPOPOPOPOP=10;
+for(int i=1;i<=POPOPOPOPOPOPOP;i++)
+valssdfsdf++;
+int sdfsdfsdfsdf=10;
+for(int i=1;i<=sdfsdfsdfsdf;i++)
+valssdfsdf++;
+not_stable441();
+
+
+
+}
+
+
+
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+ freopen("input.txt","r",stdin);
+ freopen("outpt.txt","w",stdout);
+#endif
+    io;
+    int g;cin>>g;
+ int as,ass,asss,assss,asssss,asssssss,assssssss,asssssssssss,asssssssssssssss,assssssssssssssssss;
+ int vvvv,vvvvvvv,vvvvvvvvvvv,vvvvvvvvvvvvvv,vvvvvvvvvvvvvvv,vvvvvvvvvvvvvvvvvvvvv;
+
+    for(int i=1;i<=g;i++){
+    	not_stable44();
+    }
+
+
+ return 0;
+ }
