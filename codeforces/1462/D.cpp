3f641bdd9926bd9d6755abb853ced5fc2b2dacfd
Harry Singh <harryamneet600@gmail.com>
2020-12-15 21:02:00 +0530

    Add solution for problem `D - Add to Neighbour and Remove` Link: https://codeforces.com/contest/1462/problem/D Tags: greedy, math, number theory, *1400 Ref: https://codeforces.com/contest/1462/submission/101308353

---
+++ codeforces/1462/D.cpp
@@ -0,0 +1,257 @@
+#include<bits/stdc++.h>
+#define int long long 
+#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define ipair pair<int,int>
+#define pb push_back
+#define F(i, n) for(int i=1;i<=n;i++)
+#define all(v) v.begin(),v.end()
+#define tr cout<<endl;
+#define mod 1000000007
+#define inf 1000000000000
+using namespace std;
+int string_is_the_power_But=0,AAA_AAA=0,sexa=0;
+int32_t main() {
+    io;
+  int g;cin>>g;
+    while (g--){
+        
+int n;
+cin>>n;
+
+
+      for(int I=1;I<=0;I++)AAA_AAA++;
+      AAA_AAA=2*3*4;
+        AAA_AAA+=12;
+        AAA_AAA=(2*3*4*5*3);
+        AAA_AAA+=12;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      for(int I=1;I<=0;I++)
+      {
+              int garbage_is_me=0;
+              garbage_is_me++;
+      }
+
+
+
+int a[n+1];
+   F(i,n)cin>>a[i];
+
+
+      for(int I=1;I<=0;I++)AAA_AAA++;
+      AAA_AAA=2*3*4;
+        AAA_AAA+=12;
+        AAA_AAA=(2*3*4*5*3);
+        AAA_AAA+=12;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      for(int I=1;I<=0;I++)
+      {
+              int garbage_is_me=0;
+              garbage_is_me++;
+      }
+
+
+
+
+int i=1;
+int mx=n-1;
+while(i<=n)
+{
+  int sum=0;
+    for(int j=1;j<=i;j++)sum+=a[j];
+    int c=i-1;
+    int j=i+1;
+
+
+      for(int I=1;I<=0;I++)AAA_AAA++;
+      AAA_AAA=2*3*4;
+        AAA_AAA+=12;
+        AAA_AAA=(2*3*4*5*3);
+        AAA_AAA+=12;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      for(int I=1;I<=0;I++)
+      {
+              int garbage_is_me=0;
+              garbage_is_me++;
+      }
+
+
+
+    int flag=0;
+    int t=0;
+    int val=0;
+
+
+      for(int I=1;I<=0;I++)AAA_AAA++;
+      AAA_AAA=2*3*4;
+        AAA_AAA+=12;
+        AAA_AAA=(2*3*4*5*3);
+        AAA_AAA+=12;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      for(int I=1;I<=0;I++)
+      {
+              int garbage_is_me=0;
+              garbage_is_me++;
+      }
+
+
+
+    while(j<=n)
+    {
+      t+=a[j];
+      if(t<sum)
+      {
+
+
+      for(int I=1;I<=0;I++)AAA_AAA++;
+      AAA_AAA=2*3*4;
+        AAA_AAA+=12;
+        AAA_AAA=(2*3*4*5*3);
+        AAA_AAA+=12;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      for(int I=1;I<=0;I++)
+      {
+              int garbage_is_me=0;
+              garbage_is_me++;
+      }
+
+
+
+        if(t!=a[j])++c;
+      }
+      if(t>sum){flag=1;break;}
+      if(t==sum)
+      {
+
+
+      for(int I=1;I<=0;I++)AAA_AAA++;
+      AAA_AAA=2*3*4;
+        AAA_AAA+=12;
+        AAA_AAA=(2*3*4*5*3);
+        AAA_AAA+=12;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      for(int I=1;I<=0;I++)
+      {
+              int garbage_is_me=0;
+              garbage_is_me++;
+      }
+
+
+
+        if(t!=a[j])++c;
+
+        t=0;
+      }
+      
+
+      for(int I=1;I<=0;I++)AAA_AAA++;
+      AAA_AAA=2*3*4;
+        AAA_AAA+=12;
+        AAA_AAA=(2*3*4*5*3);
+        AAA_AAA+=12;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      for(int I=1;I<=0;I++)
+      {
+              int garbage_is_me=0;
+              garbage_is_me++;
+      }
+
+
+
+      ++j;
+    }
+
+    if(t!=0)flag=1;
+
+
+
+
+      for(int I=1;I<=0;I++)AAA_AAA++;
+      AAA_AAA=2*3*4;
+        AAA_AAA+=12;
+        AAA_AAA=(2*3*4*5*3);
+        AAA_AAA+=12;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      for(int I=1;I<=0;I++)
+      {
+              int garbage_is_me=0;
+              garbage_is_me++;
+      }
+
+
+
+// cout<<i<<" "<<c<<" "<<flag<<" "<<sum;tr;
+
+
+++i;
+if(flag==0){mx=c;break;}
+}
+
+
+
+
+
+
+cout<<mx;
+tr;
+    
+        
+
+
+}
+}
