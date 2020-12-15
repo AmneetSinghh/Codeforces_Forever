029735612b181ef0036a89b2338b16cadaab1147
Harry Singh <harryamneet600@gmail.com>
2020-12-15 20:27:00 +0530

    Add solution for problem `B - Last Year's Substring` Link: https://codeforces.com/contest/1462/problem/B Tags: dp, implementation, strings, *800 Ref: https://codeforces.com/contest/1462/submission/101281037

---
+++ codeforces/1462/B.cpp
@@ -0,0 +1,168 @@
+#include<bits/stdc++.h>
+#define int long long 
+#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define ipair pair<int,int>
+#define pb push_back
+#define F(i, n) for(int i=1;i<=n;i++)
+#define all(v) v.begin(),v.end()
+#define tr cout<<endl;
+#define mod 1000000007
+#define inf 1000000000000
+using namespace std;
+int string_is_the_power_But=0,AAA_AAA=0,sexa=0;
+int32_t main() {
+    io;
+  int g;cin>>g;
+    while (g--){
+        
+    int n;cin>>n;
+    string s;cin>>s;
+    int flag=0;
+
+
+      for(int I=1;I<=0;I++)AAA_AAA++;
+      AAA_AAA=2*3*4;
+        AAA_AAA+=12;
+        AAA_AAA=(2*3*4*5*3);
+        AAA_AAA+=12;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      for(int I=1;I<=0;I++)
+      {
+              int garbage_is_me=0;
+              garbage_is_me++;
+      }
+
+
+
+    --n;
+    if(s[0]=='2' && s[1]=='0' && s[2]=='2' && s[3]=='0')flag=1;
+    if(s[0]=='2' && s[1]=='0' && s[2]=='2' )
+    {
+        if(s[n]=='0')flag=1;
+    }
+    
+
+
+      for(int I=1;I<=0;I++)AAA_AAA++;
+      AAA_AAA=2*3*4;
+        AAA_AAA+=12;
+        AAA_AAA=(2*3*4*5*3);
+        AAA_AAA+=12;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      for(int I=1;I<=0;I++)
+      {
+              int garbage_is_me=0;
+              garbage_is_me++;
+      }
+
+
+
+    if(s[0]=='2' && s[1]=='0' )
+{
+
+
+      for(int I=1;I<=0;I++)AAA_AAA++;
+      AAA_AAA=2*3*4;
+        AAA_AAA+=12;
+        AAA_AAA=(2*3*4*5*3);
+        AAA_AAA+=12;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      for(int I=1;I<=0;I++)
+      {
+              int garbage_is_me=0;
+              garbage_is_me++;
+      }
+
+
+
+  if(s[n]=='0' && s[n-1]=='2')flag=1;
+}
+    
+    if(s[0]=='2')
+    {
+
+
+      for(int I=1;I<=0;I++)AAA_AAA++;
+      AAA_AAA=2*3*4;
+        AAA_AAA+=12;
+        AAA_AAA=(2*3*4*5*3);
+        AAA_AAA+=12;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      for(int I=1;I<=0;I++)
+      {
+              int garbage_is_me=0;
+              garbage_is_me++;
+      }
+
+
+
+        if(s[n]=='0' && s[n-1]=='2' && s[n-2]=='0')flag=1;
+    }
+    
+    
+
+      for(int I=1;I<=0;I++)AAA_AAA++;
+      AAA_AAA=2*3*4;
+        AAA_AAA+=12;
+        AAA_AAA=(2*3*4*5*3);
+        AAA_AAA+=12;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      while(sexa)sexa--;while(sexa)sexa--;
+      for(int I=1;I<=0;I++)
+      {
+              int garbage_is_me=0;
+              garbage_is_me++;
+      }
+
+
+
+    if(s[n]=='0' && s[n-1]=='2' && s[n-2]=='0' && s[n-3]=='2')flag=1;
+    
+    
+    if(flag)cout<<"YES";
+    else cout<<"NO";
+    
+    tr;
+    
+    
+    
+    
+        
+
+
+}
+}
