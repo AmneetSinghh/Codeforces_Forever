a18b5c555614f64612469d9811f91e2e711d017e
Harry Singh <harryamneet600@gmail.com>
2021-03-21 18:53:00 +0530

    Add solution for problem `A - Prison Break` Link: https://codeforces.com/contest/1484/problem/A Tags: constructive algorithms, *800 Ref: https://codeforces.com/contest/1484/submission/110626680

---
+++ codeforces/1484/A.cpp
@@ -0,0 +1,31 @@
+/* not_stable but Believer */
+#include<bits/stdc++.h>
+#define int long long
+#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define ipair pair<int,int>
+#define pb push_back
+#define f(i, n) for(int i=0;i<n;i++)
+#define F(i, n) for(int i=1;i<=n;i++)
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define all(v) v.begin(),v.end()
+#define tr cout<<"\n"
+using namespace std;
+void Not_Stable()
+{
+/* Either Up or Down Practice Never Ends */
+
+int a,b;
+cin>>a>>b;
+cout<<a*b;
+tr;
+
+
+}
+
+ 
+int32_t main() {
+    io;
+    int g;cin>>g;
+    while (g--)Not_Stable();
+    return 0;
+}
