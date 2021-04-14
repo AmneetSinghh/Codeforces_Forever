55d21360366789b0dfa337f13ae5838dd4fd2622
Harry Singh <harryamneet600@gmail.com>
2021-04-15 01:20:00 +0530

    Add solution for problem `C - Hide and Seek` Link: https://codeforces.com/contest/1162/problem/C Tags: graphs, implementation, *1500 Ref: https://codeforces.com/contest/1162/submission/113041513

--- codeforces/1162/C.cpp
+++ codeforces/1162/C.cpp
@@ -4,7 +4,7 @@
 #define int unsigned long long
 #define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
 #define ipair pair<int,int>
-#define pb push_back
+#define pb insert
 #define f(i, n) for(int i=0;i<n;i++)
 #define F(i, n) for(int i=1;i<=n;i++)
 #define rep(i,a,b) for(int i=a;i<=b;i++)
@@ -15,12 +15,12 @@
 #define mod 1000000007
 #define inf 7e18
 using namespace std; 
- 
+
 const int N=100001;
- 
- 
- 
- 
+
+
+
+
 int hash1[N+1]={0};
 int vis[N+1]={0};
 int a[N+1];
@@ -28,34 +28,25 @@ set<int> vc[N+1];
 void Not_Stable(){
 int n,k;
 cin>>n>>k;
- 
+
 F(i,k){
   cin>>a[i];
   hash1[a[i]]++;
 }
- 
- 
-for(int i=1;i<=n;i++){
-  vc[i].insert(i);
-  if(i-1>=1)vc[i].insert(i-1);
-  if(i+1<=n)vc[i].insert(i+1);
-}
-int ans=0;
+
+
 for(int i=k;i>=1;i--){
   int val=a[i];
-    vector<int> ee;
-   for(auto c:vc[val]){
-    if(hash1[c])ee.push_back(c);
-   }
-   for(auto sd:ee)vc[val].erase(vc[val].find(sd));
+    vc[val].pb(val);
+    if(hash1[val-1]>0 && val-1>=1)vc[val].pb(val-1);
+    if(hash1[val+1]>0 && val+1<=n)vc[val].pb(val+1);
     hash1[val]--;
 }
- 
- 
- 
+
+int ans=(n-1)*3+1;
 for(int i=1;i<=n;i++){
-ans+=(vc[i].size());
- 
+ans-=(vc[i].size());
+
 }
 cout<<ans;
 tr;
@@ -66,7 +57,7 @@ int32_t main() {
     io;
    int g=1;
     // cin>>g;
- 
+
     while (g--)Not_Stable();
     return 0;
 }
