919cd7de8c35b2198cff90ebf614f971db55072a
Harry Singh <harryamneet600@gmail.com>
2021-06-04 20:08:00 +0530

    Add solution for problem `A - Fair Playoff` Link: https://codeforces.com/contest/1535/problem/A Tags: brute force, implementation, *800 Ref: https://codeforces.com/contest/1535/submission/118371274

---
+++ codeforces/1535/A.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+#define int long long
+#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define ipair pair<int,int>
+#define pb push_back
+#define F(i, n) for(int i=1;i<=n;i++)
+#define all(v) v.begin(),v.end()
+#define tr cout<<"\n"
+#define mod 1000000007
+#define inf 1000000000000000
+using namespace std; 
+void db(int a) {cout<<a<<"\n";}
+void db(int a,int b){ cout<<a<<" "<<b<<"\n";}
+void db(int a,int b,int c) {cout<<a<<" "<<b<<" "<<c<<"\n"; }
+void db(int a,int b,int c,int d) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<"\n";}
+void db(int a,int b,int c,int d,int e){ cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<"\n";}
+void db(int a,int b,int c,int d,int e,int f) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<"\n";}
+void db(int a,int b,int c,int d,int e,int f,int g) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<" "<<g<<"\n";}
+void db(int a,int b,int c,int d,int e,int f,int g,int h){ cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<" "<<g<<" "<<h<<"\n"; }
+#define sub substr
+
+
+int32_t main()
+{
+int t=1;
+cin>>t;
+while(t--){
+/* Practice Never Ends */
+
+
+
+int a,b,c,d;
+cin>>a>>b>>c>>d;
+
+
+int first=max(a,b);
+int second=max(c,d);
+
+
+if(min(first,second)>=min(a,b) and min(first,second)>=min(c,d))cout<<"YES";
+else cout<<"NO";
+
+
+tr;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} 
+}
+ 
+ 
+ 
