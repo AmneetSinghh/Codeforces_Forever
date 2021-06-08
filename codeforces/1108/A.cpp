db8fff838e999f06deb8fd73d8619a0b68ccca98
Harry Singh <harryamneet600@gmail.com>
2021-06-08 12:47:00 +0530

    Add solution for problem `A - Two distinct points` Link: https://codeforces.com/contest/1108/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1108/submission/118779434

---
+++ codeforces/1108/A.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#define int long long
+#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define ipair pair<int,int>
+#define pb push_back
+#define F(i, n) for(int i=1;i<=n;i++)
+#define all(v) v.begin(),v.end()
+#define tr cout<<"\n"
+#define mod 1000000007
+#define inf 1000000000000000
+using namespace std; 
+void db(int a) {cout<<a<<"\n";}
+void db(int a,int b){ cout<<a<<" "<<b<<"\n";}
+void db(int a,int b,int c) {cout<<a<<" "<<b<<" "<<c<<"\n"; }
+void db(int a,int b,int c,int d) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<"\n";}
+void db(int a,int b,int c,int d,int e){ cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<"\n";}
+void db(int a,int b,int c,int d,int e,int f) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<"\n";}
+void db(int a,int b,int c,int d,int e,int f,int g) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<" "<<g<<"\n";}
+void db(int a,int b,int c,int d,int e,int f,int g,int h){ cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<" "<<g<<" "<<h<<"\n"; }
+
+
+int32_t main()
+{
+int t=1;
+cin>>t;
+while(t--){
+/* Practice Never Ends */
+
+int flag=0;
+int l1,r1,l2,r2;
+cin>>l1>>r1>>l2>>r2;
+for(int i=l1;i<=r1;i++){
+  for(int j=l2;j<=r2;j++){
+    if(i!=j){flag=1;cout<<i<<" "<<j<<"\n";break;}
+  }
+  if(flag)break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+} 
+}
+ 
+ 
+ 
