915ee563aaf3053ee42d4c307e67aaad64a07202
Harry Singh <harryamneet600@gmail.com>
2021-06-10 16:30:00 +0530

    Add solution for problem `A - Circle Metro` Link: https://codeforces.com/contest/1169/problem/A Tags: implementation, math, *900 Ref: https://codeforces.com/contest/1169/submission/118955898

---
+++ codeforces/1169/A.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+#define int long long
+#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define ipair pair<int,int>
+#define pb push_back
+#define F(i, n) for(int i=1;i<=n;i++)
+#define all(v) v.begin(),v.end()
+#define tr cout<<"\n"
+#define mod 1000000007
+#define inf 1000000000000000
+using namespace std; 
+void db(int a) {cout<<a<<"\n";}
+void db(int a,int b){ cout<<a<<" "<<b<<"\n";}
+void db(int a,int b,int c) {cout<<a<<" "<<b<<" "<<c<<"\n"; }
+void db(int a,int b,int c,int d) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<"\n";}
+void db(int a,int b,int c,int d,int e){ cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<"\n";}
+void db(int a,int b,int c,int d,int e,int f) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<"\n";}
+void db(int a,int b,int c,int d,int e,int f,int g) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<" "<<g<<"\n";}
+void db(int a,int b,int c,int d,int e,int f,int g,int h){ cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<" "<<g<<" "<<h<<"\n"; }
+
+
+
+int32_t main()
+{
+int t=1;
+// cin>>t;
+while(t--){
+/* Practice Never Stops */
+
+int n,a,x,b,y;
+cin>>n>>a>>x>>b>>y;
+int vis[n+1]={0};
+int start=a;
+int start1=b;
+int flag=0;
+while(start!=x and start1!=y){
+
+
+
+++start;
+if(start>n)start=1;
+
+
+--start1;
+if(start1<1)start1=n;
+
+if(start==start1)flag=1;
+}
+
+
+if(flag)cout<<"YES";
+else cout<<"NO";
+
+
+
+
+
+tr;
+
+
+
+
+} 
+}
+
+
