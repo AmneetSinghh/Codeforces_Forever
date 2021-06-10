20d1b9b52bd0bfd141006a84a1ba6823369e38b4
Harry Singh <harryamneet600@gmail.com>
2021-06-10 06:04:00 +0530

    Add solution for problem `A - Choose Two Numbers` Link: https://codeforces.com/contest/1206/problem/A Tags: math, sortings, *800 Ref: https://codeforces.com/contest/1206/submission/118922345

---
+++ codeforces/1206/A.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+#define int long long
+#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define ipair pair<int,int>
+#define pb push_back
+#define F(i, n) for(int i=1;i<=n;i++)
+#define all(v) v.begin(),v.end()
+#define tr cout<<"\n"
+#define mod 1000000007
+#define inf 1000000000000000
+using namespace std; 
+void db(int a) {cout<<a<<"\n";}
+void db(int a,int b){ cout<<a<<" "<<b<<"\n";}
+void db(int a,int b,int c) {cout<<a<<" "<<b<<" "<<c<<"\n"; }
+void db(int a,int b,int c,int d) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<"\n";}
+void db(int a,int b,int c,int d,int e){ cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<"\n";}
+void db(int a,int b,int c,int d,int e,int f) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<"\n";}
+void db(int a,int b,int c,int d,int e,int f,int g) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<" "<<g<<"\n";}
+void db(int a,int b,int c,int d,int e,int f,int g,int h){ cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<" "<<g<<" "<<h<<"\n"; }
+
+
+
+int32_t main()
+{
+int t=1;
+// cin>>t;
+while(t--){
+
+
+
+
+
+
+int n;
+cin>>n;
+int a[n+1];
+set<int> s;
+F(i,n)cin>>a[i],s.insert(a[i]);
+int m;cin>>m;
+int b[m+1];
+F(i,m)cin>>b[i],s.insert(b[i]);
+
+
+for(int i=1;i<=n;i++){
+  for(int j=1;j<=m;j++)
+  {
+if(s.find(a[i]+b[j])==s.end()){cout<<a[i]<<" "<<b[j];return 0;}
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} 
+}
+
+
