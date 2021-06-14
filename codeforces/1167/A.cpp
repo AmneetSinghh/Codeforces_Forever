1d39b9a1464107c7908f6e52bfb00a54ec0977fc
Harry Singh <harryamneet600@gmail.com>
2021-06-14 21:13:00 +0530

    Add solution for problem `A - Telephone Number` Link: https://codeforces.com/contest/1167/problem/A Tags: brute force, greedy, strings, *800 Ref: https://codeforces.com/contest/1167/submission/119468379

---
+++ codeforces/1167/A.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#define int long long
+#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define ipair pair<int,int>
+#define pb push_back
+#define F(i, n) for(int i=1;i<=n;i++)
+#define all(v) v.begin(),v.end()
+#define tr cout<<"\n"
+#define mod 1000000007
+#define inf 1000000000000000
+using namespace std; 
+void db(int a) {cout<<a<<"\n";}
+void db(int a,int b){ cout<<a<<" "<<b<<"\n";}
+void db(int a,int b,int c) {cout<<a<<" "<<b<<" "<<c<<"\n"; }
+void db(int a,int b,int c,int d) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<"\n";}
+void db(int a,int b,int c,int d,int e){ cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<"\n";}
+void db(int a,int b,int c,int d,int e,int f) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<"\n";}
+void db(int a,int b,int c,int d,int e,int f,int g) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<" "<<g<<"\n";}
+void db(int a,int b,int c,int d,int e,int f,int g,int h){ cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<" "<<g<<" "<<h<<"\n"; }
+
+
+
+
+
+
+
+int32_t main()
+{
+io;
+int t=1;
+cin>>t;
+while(t--){
+/* Practice Never Stops */
+
+
+
+
+int n;
+cin>>n;
+string s;
+cin>>s;
+int start=-1;
+for(int i=0;i<n;i++){
+	if(s[i]=='8'){start=i;break;}
+}
+
+
+if(start==-1)cout<<"NO";
+else
+{
+	int m=0;
+	while(start<n){
+		++start;
+		++m;
+	}
+	if(m>=11)cout<<"YES";
+	else cout<<"NO";
+}
+
+tr;
+
+}
+}
