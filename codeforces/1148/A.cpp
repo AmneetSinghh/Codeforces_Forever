20da3cdfae510fe102ba047d51cdc25d1ddd5605
Harry Singh <harryamneet600@gmail.com>
2021-06-16 18:47:00 +0530

    Add solution for problem `A - Another One Bites The Dust` Link: https://codeforces.com/contest/1148/problem/A Tags: greedy, *800 Ref: https://codeforces.com/contest/1148/submission/119637097

---
+++ codeforces/1148/A.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+#define int long long
+#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define ipair pair<int,int>
+#define pb push_back
+#define F(i, n) for(int i=1;i<=n;i++)
+#define all(v) v.begin(),v.end()
+#define tr cout<<"\n"
+#define mod 1000000007
+#define inf 1000000000000000
+using namespace std; 
+void db(int a) {cout<<a<<"\n";}
+void db(int a,int b){ cout<<a<<" "<<b<<"\n";}
+void db(int a,int b,int c) {cout<<a<<" "<<b<<" "<<c<<"\n"; }
+void db(int a,int b,int c,int d) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<"\n";}
+void db(int a,int b,int c,int d,int e){ cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<"\n";}
+void db(int a,int b,int c,int d,int e,int f) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<"\n";}
+void db(int a,int b,int c,int d,int e,int f,int g) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<" "<<g<<"\n";}
+void db(int a,int b,int c,int d,int e,int f,int g,int h){ cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<" "<<g<<" "<<h<<"\n"; }
+
+
+
+int32_t main()
+{
+io;
+int t=1;
+// cin>>t;
+while(t--){
+/* Practice Never Stops */
+
+int a,b,c;
+cin>>a>>b>>c;
+
+
+int ans=c*2;
+int first=min(a,b);
+int second=max(a,b);
+
+ans+=(first*2);
+if(second>first)ans++;
+
+
+db(ans);
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
