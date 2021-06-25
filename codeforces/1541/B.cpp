28cfdd98d84c9c6b2dc4c3d31e71faeed8f1c6bb
Harry Singh <harryamneet600@gmail.com>
2021-06-25 21:30:00 +0530

    Add solution for problem `B - Pleasant Pairs` Link: https://codeforces.com/contest/1541/problem/B Tags: brute force, implementation, math, number theory, *1200 Ref: https://codeforces.com/contest/1541/submission/120560452

---
+++ codeforces/1541/B.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#define int long long
+#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define ipair pair<int,int>
+#define pb push_back
+#define F(i, n) for(int i=1;i<=n;i++)
+#define all(v) v.begin(),v.end()
+#define tr cout<<"\n"
+#define mod 1000000007
+#define inf 1000000000000000
+using namespace std;
+void db(int a) {cout<<a<<"\n";}
+void db(int a,int b){ cout<<a<<" "<<b<<"\n";}
+void db(int a,int b,int c) {cout<<a<<" "<<b<<" "<<c<<"\n"; }
+void db(int a,int b,int c,int d) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<"\n";}
+void db(int a,int b,int c,int d,int e){ cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<"\n";}
+void db(int a,int b,int c,int d,int e,int f) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<"\n";}
+void db(int a,int b,int c,int d,int e,int f,int g) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<" "<<g<<"\n";}
+void db(int a,int b,int c,int d,int e,int f,int g,int h){ cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<" "<<g<<" "<<h<<"\n"; }
+
+
+int32_t main()
+{
+    int t=1;
+    cin>>t;
+    while(t--){
+/* Practice Never Ends */
+
+int n;
+cin>>n;
+int a[n+1];
+unordered_map<int,int> hash;
+F(i,n)cin>>a[i],hash[a[i]]=i;
+int c=0;
+for(int i=1;i<=n*2;i++)
+{
+    if(hash.count(i)==0)continue;
+    for(int j=i+1;j<=n*2;j++)
+    {
+        if(i*j>=n*2)break;
+        if(hash.count(j)==0)continue;
+        if(hash[i]+hash[j]==i*j)++c;
+
+    }
+}
+cout<<c;
+tr;
+
+
+
+
+
+
+
+
+    }
+}
+
+
