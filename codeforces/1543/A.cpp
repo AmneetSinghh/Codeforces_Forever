7b8a4b72b58a373e32588c895cd0d41c3ab8de18
Harry Singh <harryamneet600@gmail.com>
2021-07-07 20:21:00 +0530

    Add solution for problem `A - Exciting Bets` Link: https://codeforces.com/contest/1543/problem/A Tags: greedy, math, number theory, *900 Ref: https://codeforces.com/contest/1543/submission/121584523

---
+++ codeforces/1543/A.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+#define int long long int
+#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define ipair pair<int,int>
+#define pb push_back
+#define F(i, n) for(int i=1;i<=n;i++)
+#define all(v) v.begin(),v.end()
+#define tr cout<<"\n"
+#define mod 1000000007
+#define inf 1000000000000000
+using namespace std;
+void db(int a) {cout<<a<<"\n";}
+void db(int a,int b){ cout<<a<<" "<<b<<"\n";}
+void db(int a,int b,int c) {cout<<a<<" "<<b<<" "<<c<<"\n"; }
+void db(int a,int b,int c,int d) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<"\n";}
+void db(int a,int b,int c,int d,int e){ cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<"\n";}
+void db(int a,int b,int c,int d,int e,int f) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<"\n";}
+void db(int a,int b,int c,int d,int e,int f,int g) {cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<" "<<g<<"\n";}
+void db(int a,int b,int c,int d,int e,int f,int g,int h){ cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<" "<<g<<" "<<h<<"\n"; }
+const int maxn=200005;
+
+int gcd(int a, int  b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+// Function to return LCM of two numbers
+int  lcm(int a, int b)
+{
+    return (a / gcd(a, b)) * b;
+}
+
+int32_t main()
+{
+    io;
+    int t=1;
+    cin>>t;
+    while(t--){
+/* Practice Never Ends */
+int a,b;
+cin>>a>>b;
+
+if(a==b){cout<<"0 0\n";continue;}
+int d=abs(a-b);
+
+int val=a/d;
+int first=d*val;
+int cut=abs(a-first);
+int second=d*(val+1);
+int cut1=abs(a-second);
+
+
+
+//db(first,second,cut,cut1);
+
+int ff=__gcd(a-cut,b-cut);
+int ss=__gcd(a+cut1,b+cut1);
+
+if(ff>ss)cout<<ff<<" "<<cut;
+else if(ss>ff)cout<<ss<<" "<<cut1;
+else cout<<ff<<" "<<min(cut1,cut);
+
+
+tr;
+
+
+
+
+
+
+    }
+}
+
